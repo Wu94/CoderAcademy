24e0353cafe0f93b438d57d75c1baa2590f4614e
Wu94 <wu_jeremy94@outlook.com>
2020-04-24 12:30:47 +1000

    snakebay

---
+++ RubyOnRails/snakebay/app/controllers/listings_controller.rb
@@ -0,0 +1,45 @@
+class ListingsController < ApplicationController
+    before_action :set_listing, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @listings = Listing.all
+    end
+
+    def show
+        
+    end
+
+    def new
+        @listing = Listing.new
+    end
+
+    def create
+        Lising.create(params["listing"])
+        #finish logic for creating a record
+    end
+
+    def edit
+        
+    end
+
+    def update
+       
+        #finsih logic for updating the record
+    end
+
+    def destroy
+    
+        #finish logic for deleting the record
+    end
+
+    private 
+
+    def set_listing
+        @listing = Listing.find(params[:id])
+    end
+
+    def listing_params
+        params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet, :picture)
+    end
+
+end
